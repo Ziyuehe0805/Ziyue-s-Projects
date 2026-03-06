78c83931189e41f01df7e940b6d20058c00bbf93
Ziyue He <ziyuehe4@gmail.com>
2026-03-06 14:42:19 +0000

    Modified greet method in feature branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
+
+    public static void greet() {
+    System.out.println("Hello from the feature branch!");
+}
+
 }
